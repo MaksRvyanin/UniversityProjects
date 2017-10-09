2d81034f1313eb0fdcedc21975817b816b522e8c
MaksRvyanin <mrvyanin@gmail.com>
2017-10-09 20:06:25 +0300

    Add Patterns

---
+++ LabPatterns/LabPatterns/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gui gui = new Gui();
+        }
+    }
+}
