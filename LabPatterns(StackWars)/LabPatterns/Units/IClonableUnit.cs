34c655bdfb54bc7a34fae6df23296dafdd39b5b7
MaksRvyanin <mrvyanin@gmail.com>
2017-10-11 12:23:45 +0300

    Rename project

---
+++ LabPatterns(StackWars)/LabPatterns/Units/IClonableUnit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabPatterns
+{
+    interface IClonableUnit
+    {
+        /// <summary>
+        /// Вызывается магом для клонирования юнита.
+        /// </summary>
+        /// <returns></returns>
+        UnitKeeper MagicallyClone();
+    }
+}
